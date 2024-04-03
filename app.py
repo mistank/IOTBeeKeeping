62861eaf98a25f8d778053eb22b8d3bc026cba06
mistank <milan.stankovic550@gmail.com>
2024-04-03 13:31:55 +0200

    Test commit Milan

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello World <3!'
+    return 'Hello World <3 Cileeeeee oooooo!'
 
 if __name__ == '__main__':
     app.run()
